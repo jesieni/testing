eb95c876d077066e60a415a5196b17d0c8719e9a
Aleksandra <alexandra.grudkowska@gmail.com>
2019-06-01 18:06:35 +0200

    Extracted long selector.

--- test/docteer/blocks/changeFieldName.js
+++ test/docteer/blocks/changeFieldName.js
@@ -6,7 +6,8 @@ const fill = require('../actions/fill');
 module.exports = function changeFieldType(schemaName, fieldIndex, fieldName) {
     it('should change field name to ' + fieldName, async () => {
         const driver = getDriver();
-        await click(driver, '[data-index="' + fieldIndex + '"][data-name="' + schemaName + '"] [data-test-schema-field-name]');
-        await fill(driver, '[data-index="' + fieldIndex + '"][data-name="' + schemaName + '"] [data-test-schema-field-name]', Key.chord(Key.CONTROL, 'a'), fieldName, Key.RETURN);
+        const selector = `[data-index="${fieldIndex}"][data-name="${schemaName}"] [data-test-schema-field-name]`;
+        await click(driver, selector);
+        await fill(driver, selector, Key.chord(Key.CONTROL, 'a'), fieldName, Key.RETURN);
     });
 }
